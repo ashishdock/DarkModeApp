5cc80d5dfe684760572a2615264c11c9ce29294c
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-24 16:04:04 +0530

    Dark mode programmatically and also through info.plist

--- DarkModeApp/ViewController.swift
+++ DarkModeApp/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("View did load")
+//        overrideUserInterfaceStyle = .light
+        // can do the same thing from info.plist bu changing the value of 'Appearance' key to 'Light' or 'Dark'
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("View will appear")
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("View will disappear!")
     }
 
-
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        
+        print("Trait Collection Did Change")
+        let userInterface = traitCollection.userInterfaceStyle
+        
+        if userInterface == .dark {
+            changeButton.tintColor = UIColor.white
+        } else {
+            changeButton.tintColor = .blue
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("View did disappear!")
+    }
+    
 }
 
